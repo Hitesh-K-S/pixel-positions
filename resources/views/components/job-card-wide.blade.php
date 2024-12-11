1c7c25bf6e7c57ad398c31a0abd3df3682391823
Hitesh-K-S <hiteshksheregar@gmail.com>
2024-12-11 17:20:32 +0530

    1.Created migrations 2.Models 3.seed data 4.Fetched data to front-end

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,21 +1,21 @@
+@props(['job'])
+
 <x-panel class="flex gap-x-6">    
     <div class="w-[100px] h-[100px] shrink-0">
         <x-employer-logo/>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <a href="#" class="self-start text-sm text-gray-400">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Game Developer</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">{{ $job->title }}</h3>
 
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $50,000</p>
+        <p class="text-sm text-gray-400 mt-auto">{{ $job->employment_type }} - {{ $job->salary }}</p>
     </div>
 
     <div>
-    
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-
+        @foreach ($job->tags as $tag)
+            <x-tag :tag="$tag" />            
+        @endforeach
     </div>
 </x-panel>
